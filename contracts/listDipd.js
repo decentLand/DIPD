3eb48f92497cc4b6cb0a054a0b19e0e05e726ff4
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-04-17 01:18:49 +0300

    Update listDipd.js

--- contracts/listDipd.js
+++ contracts/listDipd.js
@@ -1,5 +1,19 @@
+// a SW contract that reads a PSC cXYZ contract and append 
+// the unregistered state of votes.
+
+// REQUIREMENT:
+// the caller should hold an amount of $DLT token
+// in the wallet at the moment of execution
+
+// NOTICE:
+// this still a prototype of the planned logic. It still
+// unaudited, neither tested
+
+// Author(s):
+// @charmful0x
+
 const DECENTLAND_SWC = "sew_MAXZIgmyEPzzOTkdAca7SQCL9XTCMfxY3KOE5-M"
-const MIN_DLT_AMOUNT = 100
+const MIN_DLT_AMOUNT = 1
 
 export async function handle(state, action) {
 
@@ -11,27 +25,26 @@ export async function handle(state, action) {
 
   if (input.function === "registerUnlistedDipd") {
 
-    if (pscContract.length !== 43 || (typeof pscContract !== "string") ) {
+    if ( (pscContract.length !== 43) || (typeof pscContract !== "string") ) {
       throw new ContractError(`invalid PSC TXID`)
     }
 
     const pscContractState = await SmartWeave.contracts.readContractState(pscContract)
     const votes = pscContractState["votes"]
 
-    if ( (dipds.length !== votes.length) && (JSON.stringify(dipds) !== JSON.stringify(votes)) ) {
-      const decentlandState = await SmartWeave.contracts.readContractState(DECENTLAND_SWC)
-
-      if (decentlandState["balances"][caller] < MIN_DLT_AMOUNT) {
-          throw new ContractError(`a minimum balance of ${MIN_DLT_AMOUNT} DLT PST is required to create a dipd`)
-        }
-
-      dipds[pscContract] = votes
-
-      return {state}
+    if ( JSON.stringify(dipds[pscContract]) === JSON.stringify(votes) ) {
+      throw new ContractError(`no new votes detetcted`)
+    }
+      
+    const decentlandState = await SmartWeave.contracts.readContractState(DECENTLAND_SWC)
+    
+    if (decentlandState["balances"][caller] < MIN_DLT_AMOUNT) {
+      throw new ContractError(`a minimum balance of ${MIN_DLT_AMOUNT} DLT PST is required to create a dipd`)
     }
 
-    throw new ContractError(`no new votes detected`)
+      dipds[pscContract] = votes
+      dipds[pscContract].push(caller)
 
+      return { state }
   }
-
 }
