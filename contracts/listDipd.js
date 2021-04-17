879c5af3dcdca15bf0cb9a49b3bf9efc52449916
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-04-17 04:30:56 +0300

    feature: cXYZ PSC validation

--- contracts/listDipd.js
+++ contracts/listDipd.js
@@ -13,6 +13,7 @@
 // charmful0x
 
 const DECENTLAND_SWC = "sew_MAXZIgmyEPzzOTkdAca7SQCL9XTCMfxY3KOE5-M"
+const CXYZ_PSC_FACTORY_SRC = "ngMml4jmlxu0umpiQCsHgPX2pb_Yz6YDB8f7G6j-tpI"
 const MIN_DLT_AMOUNT = 1
 
 export async function handle(state, action) {
@@ -21,12 +22,41 @@ export async function handle(state, action) {
   const caller = action.caller
   const pscContract = input.pscSrc
   const dipds = state.dipds
+  const tagsMap = new Map()
 
 
   if (input.function === "registerUnlistedDipd") {
 
     if ( (pscContract.length !== 43) || (typeof pscContract !== "string") ) {
-      throw new ContractError(`invalid PSC TXID`)
+      throw new ContractError(`invalid TXID`)
+    }
+    
+    // using unsafeClient feature to verify that pscContract tags
+    // are compatible with a cXYZ PSC tx
+    let psc_tx = await SmartWeave.unsafeClient.transactions.get(pscContract)
+    let tags = psc_tx.get("tags")
+
+    for (let tag of tags) {
+      let key = tag.get("name", {decode: true, string: true});
+      let value = tag.get("value", {decode: true, string: true});
+      tagsMap.set(key, value)
+    }
+
+    // short-circuit validation
+    // a PSC created via CXYZ_PSC_FACTORY_SRC SW
+    // has 7 tags
+    if ( tagsMap.size !== 7 ) {
+      throw new ContractError(`invalid PSC TXID: ${pscContract}`)
+    }
+
+    if (! tagsMap.has("Contract-Src") ) {
+      throw new ContractError(`invalid PSC TXID: ${pscContract}`)
+    }
+
+    if ( tagsMap.has("Contract-Src") ) {
+      if (tagsMap.get("Contract-Src") !== CXYZ_PSC_FACTORY_SRC) {
+        throw new ContractError(`the following PSC "${pscContract}" isn't created under cXYZ`)
+      }
     }
 
     const pscContractState = await SmartWeave.contracts.readContractState(pscContract)
